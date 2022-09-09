0bd5ff44dd9282163e2e4442f5a062ced97af047
YahyaQandel <yahya.qandel@gmail.com>
2022-09-09 23:28:36 +0200

    add category application && migrations && rename field as plural in admin

---
+++ category/models.py
@@ -0,0 +1,17 @@
+from distutils.command.upload import upload
+from tabnanny import verbose
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50,unique=True)
+    slug = models.CharField(max_length=100,unique=True)
+    description = models.TextField(max_length=225,blank=True)
+    image = models.ImageField(upload_to='photos/categories',blank=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self) -> str:
+        return self.name
