9a9dafe8abc280c5e530f2efc00c51149894f598
YahyaQandel <yahya.qandel@gmail.com>
2024-07-06 21:35:58 +0300

    preview products in store page , filter by category menu on the sidebar

--- greatKart/urls.py
+++ greatKart/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
 """
 from inspect import stack
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-
+from store import urls as store_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home,name='home'),
+    path('store/',include(store_urls)),
     
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
--- store/views.py
+++ store/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Product,Category
 
 # Create your views here.
+
+def store(request,category_slug=None):
+    products = Product.objects.filter(is_available=True)
+    categories = Category.objects.all()
+    if category_slug:
+        category = Category.objects.filter(slug = category_slug)
+        products = products.filter(category__in=category) if category else products
+    context = {
+        'products' : products,
+        'categories' : categories
+    }
+    return render(request,"store.html",context=context)
